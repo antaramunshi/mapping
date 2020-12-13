e3c5e8eb0754525ee54d980b421303c25cc92211
antaramunshi <74422097+antaramunshi@users.noreply.github.com>
2020-12-13 13:18:13 +0530

    changing font style

--- map.py
+++ map.py
@@ -5,9 +5,15 @@ data = pandas.read_csv("Volcanoes.csv")
 lat= list(data["LAT"])
 lon= list(data["LON"])
 elev= list(data["ELEV"])
+
+html = """<h4>Volcano information:</h4>
+Height: %s m
+"""
+
 map = folium.Map(location= [40,-100], tiles= "Stamen Terrain ")
 fg= folium.FeatureGroup(name= "My Map")
 for lt, ln, el in zip (lat,lon,elev):
+    iframe = folium.IFrame(html=html % str(el), width=200, height=100)
     fg.add_child(folium.Marker(location = [lt , ln ], popup=str(el)+" m" , icon=folium.Icon(color='green')))
 
 map.add_child(fg)
