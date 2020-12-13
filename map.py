ee4f303980179285f3a4742879496a55cee6934e
antaramunshi <74422097+antaramunshi@users.noreply.github.com>
2020-12-13 13:31:21 +0530

    changing colour of the elevation points
    Changing the elevation the basis of height . For eg if the height is less than 1000 it is represesnted by green if between 1000 to 3000 then by orange and lastly if it is greater than 3000 it is represented by red

--- map.py
+++ map.py
@@ -6,15 +6,25 @@ lat= list(data["LAT"])
 lon= list(data["LON"])
 elev= list(data["ELEV"])
 
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000<= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
 html = """<h4>Volcano information:</h4>
 Height: %s m
 """
 
 map = folium.Map(location= [40,-100], tiles= "Stamen Terrain ")
 fg= folium.FeatureGroup(name= "My Map")
+
 for lt, ln, el in zip (lat,lon,elev):
     iframe = folium.IFrame(html=html % str(el), width=200, height=100)
-    fg.add_child(folium.Marker(location = [lt , ln ], popup=str(el)+" m" , icon=folium.Icon(color='green')))
+    fg.add_child(folium.Marker(location = [lt , ln ], popup=str(el)+" m" , icon=folium.Icon(color= color_producer(el))))
 
 map.add_child(fg)
+
 map.save("Map1.html")
